6f976fa80c481897316843bc16fba1fc624b51b8
Dan <danielcgilbert@gmail.com>
2010-10-29 11:00:03 -0500

    Tab issue.

--- app/models/keyword_filter_tags.rb
+++ app/models/keyword_filter_tags.rb
@@ -1,33 +1,33 @@
 module KeywordFilterTags
-	include Radiant::Taggable
+  include Radiant::Taggable
 
-	desc %{
+  desc %{
 <p>Process content only if the current page contains the keywords specified.</p>
 <p> The attribute "all" controls if you want all of them or just one of keywords to match, by all of the keywords specifieds are required. </p>
 <p> The attribute "case_sensitive" controls if you want matches to be case sensitive. True by default.</p>
 *Usage*:
 <pre><code><r:if_keyword filter="keyword1 keyword2" [all="false"] [case_sensitive="true"]> ... </r:if_keyword></code></pre>
-	}
-	tag "if_keyword" do |tag|
-		if (isOk?(tag)) then
-			tag.expand
-		end
-	end
+  }
+  tag "if_keyword" do |tag|
+    if (isOk?(tag)) then
+      tag.expand
+    end
+  end
 
-	desc %{
+  desc %{
 <p>Process content only if the current page <em>does not</em> contains the keywords specified. This tag does the exact inverse of the if_keyword tag.</p>
 <p> The attribute "all" controls if you want all of them or just one of keywords to match, by all of the keywords specifieds are required. </p>
 <p> The attribute "case_sensitive" controls if you want matches to be case sensitive. True by default.</p>
 *Usage*:
 <pre><code><r:unless_keyword filter="keyword1 keyword2" [all="false"] [case_sensitive="true"]> ... </r:unless_keyword></code></pre>
-	}
-	tag "unless_keyword" do |tag|
-		if (not isOk?(tag)) then
-			tag.expand
-		end
-	end
+  }
+  tag "unless_keyword" do |tag|
+    if (not isOk?(tag)) then
+      tag.expand
+    end
+  end
 
-	def isOk?(tag)
+  def isOk?(tag)
     all_attr            = tag.attr["all"]
     filter_attr         = tag.attr["filter"]
     case_attr = tag.attr["case_sensitive"]
@@ -46,10 +46,10 @@ module KeywordFilterTags
     end
 
     filters.each do |keyword|
-			if (keywords.include?(keyword) != all) then
-				return(not all)
-			end
-		end
-		return all
-	end
+      if (keywords.include?(keyword) != all) then
+        return(not all)
+      end
+    end
+    return all
+  end
 end
